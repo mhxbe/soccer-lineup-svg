03afe9646e7fddb98ec81a099b87e765d64eb38e
Mike Henderyckx <mike.henderyckx@gmail.com>
2019-07-30 23:42:58 +0200

    Add cat.jpg & soccer.svg to test if we can display an image/svg. Also update README.md

--- README.md
+++ README.md
@@ -14,4 +14,14 @@ I was pitching this vague idea to my awesome colleague, Rob, and he immediately 
 
 ## Url
 
-Currenlty, the testing url is [http://localhost:9069/lineup?formation=433&players=1-2-3-4-5-6-7-8-9-10-11](http://localhost:9069/lineup?formation=433&players=1-2-3-4-5-6-7-8-9-10-11). This will probably change once we optimize this app.
+Currently, the testing url is [http://localhost:9069/lineup?formation=433&players=1-2-3-4-5-6-7-8-9-10-11](http://localhost:9069/lineup?formation=433&players=1-2-3-4-5-6-7-8-9-10-11). This will output the formation & players in JSON. The form of the url will probably change to make it more accessible/shareable.
+
+## Displaying image/sv
+
+We can display an image or svg by following links:
+
+- cat.jpg: [http://localhost:9069/lineup?formation=cat&players=9000](http://localhost:9069/lineup?formation=cat&players=9000)
+
+- soccer.svg [http://localhost:9069/lineup?formation=svg&players=9000](http://localhost:9069/lineup?formation=svg&players=9000)
+
+This means the possibilities are endless for our soccer lineup svg generator.
--- app/lineup.controller.js
+++ app/lineup.controller.js
@@ -1,7 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+
+function readImage(asset, res) {
+  const mapper = {
+    cat: { link: '/cat.jpg', mime: 'image/jpeg' },
+    svg: { link: '/soccer.svg', mime: 'image/svg+xml' },
+  };
+  const img = mapper[asset];
+
+  const dir = path.join(__dirname, 'images');
+  const file = path.join(dir, img.link);
+  const stream = fs.createReadStream(file);
+
+  stream.on('open', () => {
+    res.set('Content-Type', img.mime);
+    stream.pipe(res);
+  });
+
+  stream.on('error', () => {
+    res.set('Content-Type', 'text/plain');
+    res.status(404).end('Image not found or generated yet.');
+  });
+}
+
 export const generateLineup = (req, res) => {
   const { formation: formationString, players: playersString } = req.query;
   const formation = formationString.split('');
   const players = playersString.split('-');
 
-  return res.status(200).json({ formation, players });
+  if (formationString === 'cat' || formationString === 'svg') {
+    readImage(formationString, res);
+  } else {
+    return res.status(200).json({ formation, players });
+  }
 };
